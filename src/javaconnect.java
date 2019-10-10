82d2ec8593c1e49d391f3976a3568ff34743d888
Saket Kumar Pandey <56403426+saket2309@users.noreply.github.com>
2019-10-10 21:17:15 +0530

    Add files via upload

---
+++ src/javaconnect.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author HP X360
+ */
+import java.sql.*;
+import javax.swing.JOptionPane;
+
+public class javaconnect {
+  Connection conn;
+  
+  public static Connection ConnecrDB(){
+      try{
+          Class.forName("org.sqlite.JDBC");
+          Connection conn=DriverManager.getConnection("jdbc:sqlite:C:\\Users\\HP X360\\Documents\\NetBeansProjects\\ Library Management System\\LibraryNew.sqlite");
+          return conn;        
+      }catch(Exception e){
+          JOptionPane.showMessageDialog(null, e);
+          return null;
+  }
+}
+}
